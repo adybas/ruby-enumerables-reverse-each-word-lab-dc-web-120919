ca540ce16fac633104c4b412860fbd1316207f18
Anna Dybas <a.m.dybas@gmail.com>
2019-12-05 00:47:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,8 @@ def reverse_each_word(string)
 #    reversed_array << word.reverse
 #  end
 #  reversed_array.join(" ")
-  reversed_array = []
+
   string.split(" ").collect do |word|
-    reversed_array<<word.reverse
+    reversed_array<<word.reverse.join(" ")
   end
-  reversed_array.join(" ")
 end
